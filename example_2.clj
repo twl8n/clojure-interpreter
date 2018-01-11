37a9ff127448748f22919eef5c488f0bc12350dc
Tom Laudeman <tlaudeman@roomkey.com>
2018-01-11 02:12:42 +0000

    Tweak example_2

--- example_2.clj
+++ example_2.clj
@@ -1,11 +1,9 @@
 (ns example_2
   (:import [javax.imageio ImageIO]))
 
-;; We don't have to require clojure.java.io because it is required by core.clj. 
-
-;; This example demonstrates :import of java packages. FWIW, the ImageIO/read function is dreadfully slow, and fails
-;; to read some jpeg files. If you want to do this for real, install ImageMagick and run the identify utility
-;; via a shell call.
+;; This example demonstrates :import of java packages. FWIW, the ImageIO/read function is dreadfully slow, and
+;; fails to read some jpeg files. If you want to do this for real, install ImageMagick and run the identify
+;; utility via a shell call.
 
 (defn main
   []
