bc39dd230f93036123c39c6991bb0e45ebef30a0
twl8n <tom@laudeman.com>
2018-08-13 19:40:42 -0400

    Fix local_release.clj to create user bin dir if necessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -e clojint.jar ]]
 then
-    echo "Using ./clojint.tar"
+    echo "Using ./clojint.jar"
     java -jar clojint.jar $@
 else
     echo "Using target/uberjar/clojint-0.1.0-SNAPSHOT-standalone.jar"
