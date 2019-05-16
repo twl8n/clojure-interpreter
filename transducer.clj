d87a360719abee759baf8962cacf37f681b8f7dd
Tom Laudeman <tlaudeman@roomkey.com>
2019-05-16 16:48:20 +0000

    Add more detail about transducers

--- transducer.clj
+++ transducer.clj
@@ -3,7 +3,7 @@
 ;; usage:
 ;; clj transducer.clj
 
-(def myvec (range 10000))
+(def myvec (range 11))
 
 (defn bar [xx]
   (->> xx
@@ -24,6 +24,23 @@
    (partial map #(- % 0.1))
    (partial map #(/ % 10.0))
    (partial map #(+ 3 %))))
+
+;; Calling rxr via the transduce in comment will first call rxr with no args returning []
+;; Then it is called with the previous return and each element of myvec until myvec is exhausted
+;; Finally, it is called with the finaly return as a single arg.
+(defn rxr
+  ([] (prn "no args") [])
+  ([xx] (prn "one arg" xx) xx)
+  ([xx yy] (prn "two args" xx yy) (conj xx yy)))
+
+(defn rxn [& args]
+  (when (= 2 (count args)) (prn "yy:" (second args)))
+  nil)
+
+(comment
+  (transduce foo rxr myvec)
+  (transduce foo rxn myvec)
+  )
 
 (print "thread-last: " (with-out-str (time (def bar-res (doall (bar myvec))))))
 
